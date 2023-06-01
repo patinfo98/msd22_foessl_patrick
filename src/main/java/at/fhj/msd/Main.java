b29f24905e9a2b139652be87f7e69230764bf0d0
Patrick Foessl <patrick.foessl@edu.fh-joanneum.at>
2023-06-01 12:25:24 +0200

    added info and error logger to the program

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,11 +1,21 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        logger.info("Info Log");
         calculator.add(1,1);
         calculator.minus(1,1);
         calculator.multiply(1,1);
         calculator.divide(1,1);
+        logger.error("Error Log");
+
+
+
+
     }
 }
